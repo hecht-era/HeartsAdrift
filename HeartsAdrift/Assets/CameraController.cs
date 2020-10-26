34a6c6ab975f2d514210ebf9237d19f594648718
hecht-era <33850268+hecht-era@users.noreply.github.com>
2020-10-26 11:50:34 -0700

    docking/undocking/disembarking

---
+++ HeartsAdrift/Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float lookSpeed = 3;
+    private Vector2 rotation = Vector2.zero;
+    public void Update() // Look rotation (UP down is Camera) (Left right is Transform rotation)
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        transform.eulerAngles = new Vector2(rotation.x, rotation.y) * lookSpeed;
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, rotation.y * lookSpeed, 0);
+    }
+}
