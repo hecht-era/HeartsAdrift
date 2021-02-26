51184b48cb09ca837be7547eca001de8e3feb6b3
Chase Barton <59599993+ChaseBarton@users.noreply.github.com>
2021-02-25 19:09:34 -0800

    Add files via upload

---
+++ HeartsAdrift/Assets/Treasure.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : MonoBehaviour
+{
+    [SerializeField] public GameObject treasure;
+    Animator anim;
+
+    void Start()
+    {
+        anim = transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool CollectTreasure()
+    {
+        treasure.SetActive(true);
+        anim.Play("Control");
+        return true;
+    }
+
+    public void TreasureDone()
+    {
+        treasure.SetActive(false);
+        anim.Play("Release");
+    }
+}
