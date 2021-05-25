c5a9390961476fa91eba173c33921f146b148b78
Chase <chasebarton381@gmail.com>
2021-05-24 19:44:47 -0700

    Put in Main Character and Updated the Windmills

--- HeartsAdrift/Assets/Scripts/WheelRotation.cs
+++ HeartsAdrift/Assets/Scripts/WheelRotation.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class WheelRotation : MonoBehaviour
 {
     // rotation speed in degrees per second.
-    public float rotationSpeed = 5f;
+    public float rotationSpeedX = 5f;
+    public float rotationSpeedZ = 0f;
 
     void Update()
     {
         // Rotate around X Axis
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeedX * Time.deltaTime);
+        // Rotate around Z Axis
+        transform.Rotate(Vector3.forward * rotationSpeedZ * Time.deltaTime);
     }
 }
